0f096dd66b6d4e1ae89b78212eb1d77bcced7348
Mayank Bisht <mayankbisht17082004@gmail.com>
2025-03-31 06:02:26 +0530

    Added authorization middleware and Cloudinary integration for profile picture upload

---
+++ backend/src/lib/cloudinary.js
@@ -0,0 +1,13 @@
+// Configures and exports Cloudinary for image uploads
+
+import {v2 as cloudinary} from "cloudinary"; //CLoudinary's v2 API as cloudinary 
+import {config} from "dotenv";
+
+config();
+cloudinary.config({
+    cloud_name:process.env.CLOUDINARY_CLOUD_NAME,
+    api_key:process.env.CLOUDINARY_API_KEY,
+    api_secret:process.env.CLOUDINARY_API_SECRET,
+});
+
+export default cloudinary;

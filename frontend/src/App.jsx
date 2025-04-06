fe024f18d7e38d4a84ed8522d3138ea03f0ff30d
Mayank Bisht <mayankbisht17082004@gmail.com>
2025-04-06 08:56:22 +0530

    Add and style Login/Signup pages with form validation and AuthImagePattern

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -8,6 +8,7 @@ import SettingsPage from "./pages/SettingsPage"
 import ProfilePage from "./pages/ProfilePage"
 import {useAuthStore} from "./store/useAuthStore.js"
 import { Loader } from "lucide-react"
+import { Toaster } from "react-hot-toast"
 
 const App = () => {
 
@@ -38,6 +39,7 @@ const App = () => {
           <Route path="/settings" element={<SettingsPage />} />
           <Route path="/profile" element={authUser ? <ProfilePage /> : <Navigate to="/login"/>} />
         </Routes>
+        <Toaster/>
       </div>
     </div>
   );
--- frontend/src/store/useAuthStore.js
+++ frontend/src/store/useAuthStore.js
@@ -1,5 +1,6 @@
 import { create } from "zustand";
 import { axiosInstance } from "../lib/axios.js"
+import toast from "react-hot-toast"
 
 //custom Zustand hook you can use in any React component to access or update auth-related state
 export const useAuthStore = create((set) => ({
@@ -20,6 +21,47 @@ export const useAuthStore = create((set) => ({
         }
         finally {
             set({ isCheckingAuth: false });
+        }
+    },
+
+    signup: async (data) =>{
+        set({isSigningUp: true});
+        try{
+            const res = await axiosInstance.post("/auth/signup", data);
+            set({authUser: res.data});
+            toast.success("Account created successfully");
+        }
+        catch(error){
+            toast.error(error.response.data.message);
+        }
+        finally{
+            set({isSigningUp: false});
+        }
+    },
+
+    logout: async () =>{
+        try{
+            await axiosInstance.post("/auth/logout");
+            set({authUser: null});
+            toast.success("Logged out Successfully");
+        }
+        catch(error){
+            toast.error(error.response.data.message);
+        }
+    },
+
+    login: async (data) =>{
+        set({isLoggingIn: true});
+        try{
+            const res = await axiosInstance.post("/auth/login", data);
+            set({authUser: res.data});
+            toast.success("Logged In successfully");
+        }
+        catch(error){
+            toast.error(error.response.data.message);
+        }
+        finally{
+            set({isLoggingIn: false});
         }
     }
 }))

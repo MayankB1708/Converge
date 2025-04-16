5b275ec464d35e04251333570755b7dc192608ac
Mayank Bisht <mayankbisht17082004@gmail.com>
2025-04-16 12:07:55 +0530

    Integrated Socket.IO for real-time chat and added seed for testing

--- backend/src/index.js
+++ backend/src/index.js
@@ -5,8 +5,9 @@ import dotenv from "dotenv"
 import { connectDB } from "./lib/db.js"
 import cookieParser from "cookie-parser"
 import cors from "cors"
+import {app, server} from "./lib/socket.js"
 
-const app = express();
+
 
 dotenv.config()
 const PORT = process.env.PORT
@@ -24,7 +25,7 @@ app.use(cors({
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 
-app.listen(PORT, () => {
+server.listen(PORT, () => {
     console.log("Server is running on port:" + PORT);
     connectDB()
 });
--- backend/src/middleware/auth.middleware.js
+++ backend/src/middleware/auth.middleware.js
@@ -16,12 +16,12 @@ export const protectRoute = async (req,res,next) => {
 
         const decoded = jwt.verify(token, process.env.JWT_SECRET);
         if(!decoded){
-            res.status(401).json({message: "Unauthorized - Invalid Token"});
+            return res.status(401).json({message: "Unauthorized - Invalid Token"});
         }
         const user = await User.findById(decoded.userId).select("-password");
 
         if(!user){
-            res.status(404).json({message:"User not found"});
+            return res.status(404).json({message:"User not found"});
         }
 
         req.user = user;
